615d31b11c9eeaf1c754418b67a6fc4364b96542
coder-rakib-62 <abdulmajidrakib908@gmail.com>
2026-01-14 17:08:55 +0600

    push

---
+++ js/custom.js
@@ -0,0 +1,97 @@
+// let x = 50;
+// console.log(x);
+
+// let score = 10;
+// score += 5;
+// console.log(score);
+
+// let balance = 1000;
+// balance -= 275;
+// console.log(balance);
+
+// let price = 120;
+// price *= 3;
+// console.log(price);
+
+// let total = 500;
+// total /= 5;
+// console.log(total);
+
+// let number = 29;
+// number %= 2;
+// console.log(number);
+
+// let base = 2;
+// base **= 5;
+// console.log(base);
+
+// let marks = 40;
+// marks += 10;
+// marks *= 2;
+// marks /= 5;
+// console.log(marks);
+
+// js comparison operator
+
+let a = 10;
+let b = "10";
+console.log(a == b);
+console.log(a === b);
+console.log(a != b);
+console.log(a !== b);
+console.log(a > 5);
+
+// js logical operator
+
+// let userName = "Rakib";
+// let password = "12345";
+// if (userName && password) {
+//   console.log("login success");
+// } else {
+//   console.log("login failed");
+// }
+
+// let age = 20;
+// let hasID = true;
+// let isVIP = false;
+// if (age >= 18 && (hasID || isVIP)) {
+//   console.log("special entry allowed");
+// } else {
+//   console.log("no entry");
+// }
+
+// let age = 18;
+// let isCitizen = true;
+// if (age >= 18 && isCitizen) {
+//   console.log("you can vote");
+// } else {
+//   console.log("you cant vote");
+// }
+
+// const myRice = 70;
+// const myBudget = 50;
+
+// if (myRice <= myBudget) {
+//   console.log("ami eita kinte prbo");
+// } else {
+//   console.log("ami eita kinte prbo na ");
+// }
+
+// grade example
+let score = 32;
+
+if (score >= 80) {
+  console.log("your grade is A+");
+} else if (score >= 70) {
+  console.log("your grade is A");
+} else if (score >= 60) {
+  console.log("Your grade is A-");
+} else if (score >= 50) {
+  console.log("your grade is B");
+} else if (score >= 40) {
+  console.log("your grade is D");
+} else if (score >= 33) {
+  console.log("your grade is C ");
+} else {
+  console.log("Failed");
+}
